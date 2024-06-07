f4699bb5d18df1165ea1850413dc50a7da86dcbc
q <brianthompson080@gmail.com>
2024-06-07 18:14:44 -0400

    1st commit

--- features/environment.py
+++ features/environment.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.options import Options
 from webdriver_manager.firefox import GeckoDriverManager
 
 from app.application import Application
@@ -11,13 +12,13 @@ def browser_init(context):
     """
     :param context: Behave context
     """
-    #driver_path = ChromeDriverManager().install()
-    #service = Service(driver_path)
-    #context.driver = webdriver.Chrome(service=service)
-
-    driver_path = GeckoDriverManager().install()
+    driver_path = ChromeDriverManager().install()
     service = Service(driver_path)
-    context.driver = webdriver.Firefox(service=service)
+    context.driver = webdriver.Chrome(service=service)
+
+    #driver_path = GeckoDriverManager().install()
+    #service = Service(driver_path)
+    #context.driver = webdriver.Firefox(service=service)
 
     ### BROWSERS WITH DRIVERS: provide path to the driver file ###
     #service = Service(executable_path='/Users/svetlanalevinsohn/careerist/18-python-selenium-automation/geckodriver')
@@ -32,6 +33,22 @@ def browser_init(context):
    #     service=service
    # )
 
+    ### BROWSERSTACK ###
+    # Register for BrowserStack, then grab it from https://www.browserstack.com/accounts/settings
+    bs_user = 'brianthompson_QjCRxc'
+    bs_key = 'syDNvJAqWUVmUcXs2bWm'
+    url = f'http://{bs_user}:{bs_key}@hub-cloud.browserstack.com/wd/hub'
+    #
+    options: Options = Options()
+    bstack_options = {
+         'osVersion': '13.0',
+         'deviceName': 'Samsung Galaxy S23',
+         'browserName': 'chrome',
+         'sessionName': 'Internship_Test_Case_1'
+     }
+    options.set_capability('bstack:options', bstack_options)
+    context.driver = webdriver.Remote(command_executor=url, options=options)
+
     context.driver.maximize_window()
 
     context.driver.maximize_window()
@@ -40,20 +57,16 @@ def browser_init(context):
 
     context.app = Application(context.driver)
 
-
 def before_scenario(context, scenario):
     print('\nStarted scenario: ', scenario.name)
     browser_init(context)
 
-
 def before_step(context, step):
     print('\nStarted step: ', step)
-
 
 def after_step(context, step):
     if step.status == 'failed':
         print('\nStep failed: ', step)
-
 
 def after_scenario(context, feature):
     context.driver.delete_all_cookies()
